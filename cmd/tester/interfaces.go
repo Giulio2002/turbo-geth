erigontech/erigon

Apply Turbo-Geth modifications to go-ethereum codebase

cmd/tester/interfaces.go
@@ -1,0 +1,17 @@
+package main
+
+import (
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+)
+
+type BlockFeeder interface {
+	Close()
+	GetHeaderByHash(hash common.Hash) *types.Header
+	GetHeaderByNumber(number uint64) *types.Header
+	GetBlockByHash(hash common.Hash) (*types.Block, error)
+	TotalDifficulty() *big.Int
+	LastBlock() *types.Block
+}

Giulio2002/turbo-geth

cmd/tester/interfaces.go
@@ -1,0 +1,17 @@
+package main
+
+import (
+	"math/big"
+
+	"github.com/ledgerwatch/turbo-geth/common"
+	"github.com/ledgerwatch/turbo-geth/core/types"
+)
+
+type BlockFeeder interface {
+	Close()
+	GetHeaderByHash(hash common.Hash) *types.Header
+	GetHeaderByNumber(number uint64) *types.Header
+	GetBlockByHash(hash common.Hash) (*types.Block, error)
+	TotalDifficulty() *big.Int
+	LastBlock() *types.Block
+}